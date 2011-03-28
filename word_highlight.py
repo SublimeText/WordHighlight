4783abac0544348b766de11975f2cb9bc7a9114f
Nikolaus Wittenstein <nikolaus.wittenstein@gmail.com>
2011-03-28 10:36:59 -0700

    Add option to highlight current word when selection is empty

--- word_highlight.py
+++ word_highlight.py
@@ -4,6 +4,11 @@ DRAW OUTLINED
 		Add this to your user prefs to make the highlights be drawn as outlines
 		instead of as filled highlights.
 
+HIGHLIGHT WORDS WHEN SELECTION IS EMPTY
+	"word_highlights_when_selection_is_empty" : true
+		Add this to your user prefs to make words highlight when the insertion point
+		is inside of them but they're not actually selected.
+
 COLOR OPTIONS
 	"word_highlights_color_scope_name" : "wordhighlight"
 		Normally the color of the highlights is the same as the color of comments
@@ -35,6 +40,7 @@ class WordHighlightListener(sublime_plugin.EventListener):
 		settings = view.settings()
 		color_scope_name = settings.get('word_highlights_color_scope_name', DEFAULT_COLOR_SCOPE_NAME)
 		draw_outlined = bool(settings.get('word_highlights_draw_outlined')) * sublime.DRAW_OUTLINED
+		word_separators = settings.get('word_separators')
 		
 		regions = []
 		for sel in view.sel():
@@ -46,5 +52,9 @@ class WordHighlightListener(sublime_plugin.EventListener):
 			if len(sel) == len(view.word(sel)):
 				string = view.substr(sel).strip()
 				if len(string):
+					regions += view.find_all(string, sublime.LITERAL)
+			elif len(sel) == 0 and bool(settings.get('word_highlights_when_selection_is_empty')):
+				string = view.substr(view.word(sel)).strip()
+				if len(string) and all([not c in word_separators for c in string]):
 					regions += view.find_all(string, sublime.LITERAL)
 		view.add_regions("WordHighlight", regions, color_scope_name, draw_outlined)
