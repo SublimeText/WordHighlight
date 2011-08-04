248961d4074093caffdc60ca7650fd29c2e23c17
Nikolaus Wittenstein <nikolaus@synapse.com>
2011-08-03 18:03:05 -0700

    Move settings into a package-specific settings file

--- word_highlight.py
+++ word_highlight.py
@@ -1,20 +1,20 @@
 """
 DRAW OUTLINED
-	"word_highlights_draw_outlined" : true
-		Add this to your user prefs to make the highlights be drawn as outlines
-		instead of as filled highlights.
+	"draw_outlined" : true
+		This makes the highlights be drawn as outlines instead of as filled
+		highlights.
 
 HIGHLIGHT WORDS WHEN SELECTION IS EMPTY
-	"word_highlights_when_selection_is_empty" : true
-		Add this to your user prefs to make words highlight when the insertion point
-		is inside of them but they're not actually selected.
+	"highlight_when_selection_is_empty" : true
+		This makes words highlight when the insertion point is inside of them but
+		when they're not actually selected.
 
 COLOR OPTIONS
-	"word_highlights_color_scope_name" : "wordhighlight"
-		Normally the color of the highlights is the same as the color of comments
-		in your code. If you'd like to customize the color, add the below to your
-		color scheme file and change EDF2E9 to whatever color you want, then add
-		this to your user file preferences.
+	"color_scope_name" : "wordhighlight"
+		Normally the color of the highlights is the same as the color of comments in
+		your code. If you'd like to customize the color, add the below to your color
+		scheme file and change EDF2E9 to whatever color you want, then change
+		color_scope_name to the scope name in the block you added.
 ---ADD TEXT BELOW THIS LINE TO YOUR COLOR SCHEME FILE---
 		<dict>
 				<key>name</key>
@@ -46,10 +46,11 @@ def regex_escape(string):
 
 class WordHighlightListener(sublime_plugin.EventListener):
 	def on_selection_modified(self,view):
-		settings = view.settings()
-		color_scope_name = settings.get('word_highlights_color_scope_name', DEFAULT_COLOR_SCOPE_NAME)
-		draw_outlined = bool(settings.get('word_highlights_draw_outlined')) * sublime.DRAW_OUTLINED
-		word_separators = settings.get('word_separators')
+		word_separators = view.settings().get('word_separators')
+		
+		settings = sublime.load_settings('Word Highlight.sublime-settings')
+		color_scope_name = settings.get('color_scope_name', DEFAULT_COLOR_SCOPE_NAME)
+		draw_outlined = bool(settings.get('draw_outlined')) * sublime.DRAW_OUTLINED
 		
 		regions = []
 		for sel in view.sel():
@@ -62,7 +63,7 @@ class WordHighlightListener(sublime_plugin.EventListener):
 				string = view.substr(sel).strip()
 				if len(string):
 					regions += view.find_all('\\b'+regex_escape(string)+'\\b')
-			elif len(sel) == 0 and bool(settings.get('word_highlights_when_selection_is_empty')):
+			elif len(sel) == 0 and bool(settings.get('highlight_when_selection_is_empty')):
 				string = view.substr(view.word(sel)).strip()
 				if len(string) and all([not c in word_separators for c in string]):
 					regions += view.find_all('\\b'+regex_escape(string)+'\\b')
