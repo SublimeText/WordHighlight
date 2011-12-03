1c86b7b94dd767b5905816e2038bb961a897e90a
Liam Cain <contactliam529@gmail.com>
2011-12-03 16:55:43 -0500

    Added message in status bar with the number of occurrences

--- word_highlight.py
+++ word_highlight.py
@@ -6,60 +6,65 @@ from threading import Timer
 settings = sublime.load_settings('Word Highlight.sublime-settings')
 
 class Pref:
-	def load(self):
-		Pref.color_scope_name               	= settings.get('color_scope_name', "comment")
-		Pref.selection_delay                    = settings.get('selection_delay', 0.04)
-		Pref.draw_outlined                    	= bool(settings.get('draw_outlined', True)) * sublime.DRAW_OUTLINED
-		Pref.highlight_when_selection_is_empty	= bool(settings.get('highlight_when_selection_is_empty', True))
-		Pref.word_separators                  	= []
+    def load(self):
+        Pref.color_scope_name                   = settings.get('color_scope_name', "comment")
+        Pref.selection_delay                    = settings.get('selection_delay', 0.04)
+        Pref.draw_outlined                      = bool(settings.get('draw_outlined', True)) * sublime.DRAW_OUTLINED
+        Pref.highlight_when_selection_is_empty  = bool(settings.get('highlight_when_selection_is_empty', True))
+        Pref.word_separators                    = []
 
-Pref().load();
+Pref().load()
 
 settings.add_on_change('color_scope_name',                  lambda:Pref().load())
 settings.add_on_change('draw_outlined',                     lambda:Pref().load())
 settings.add_on_change('highlight_when_selection_is_empty', lambda:Pref().load())
 
 def delayed(seconds):
-	def decorator(f):
-		def wrapper(*args, **kargs):
-			if wrapper.timer:
-				wrapper.timer.cancel()
-				wrapper.timer = None
-			wrapper.timer = Timer(seconds, f, args, kargs)
-			wrapper.timer.start()
-		wrapper.timer = None
-		return wrapper
-	return decorator
+    def decorator(f):
+        def wrapper(*args, **kargs):
+            if wrapper.timer:
+                wrapper.timer.cancel()
+                wrapper.timer = None
+            wrapper.timer = Timer(seconds, f, args, kargs)
+            wrapper.timer.start()
+        wrapper.timer = None
+        return wrapper
+    return decorator
 
 class WordHighlightListener(sublime_plugin.EventListener):
-	prev_regions = []
+    prev_regions = []
 
-	def on_activate(self, view):
-		Pref.word_separators = view.settings().get('word_separators')
+    def on_activate(self, view):
+        Pref.word_separators = view.settings().get('word_separators')
 
-	def on_selection_modified(self, view):
-		if not view.settings().get('is_widget'):
-			self.pend_highlight_occurences(view)
+    def on_selection_modified(self, view):
+        if not view.settings().get('is_widget'):
+            self.pend_highlight_occurences(view)
 
-	@delayed(Pref.selection_delay)
-	def pend_highlight_occurences(self, view):
-		sublime.set_timeout(lambda: self.highlight_occurences(view), 0)
+    @delayed(Pref.selection_delay)
+    def pend_highlight_occurences(self, view):
+        sublime.set_timeout(lambda: self.highlight_occurences(view), 0)
 
-	def highlight_occurences(self, view):
-		regions = []
-		for sel in view.sel():
-			if sel.empty() and Pref.highlight_when_selection_is_empty:
-				string = view.substr(view.word(sel)).strip()
-				if string and all([not c in Pref.word_separators for c in string]):
-					regions += view.find_all('(?<![\\w])'+re.escape(string)+'\\b')
-			else:
-				word = view.word(sel)
-				if word.end() == sel.end() and word.begin() == sel.begin() :
-					string = view.substr(word).strip()
-					if string:
-						regions += view.find_all('(?<![\\w])'+re.escape(string)+'\\b')
-		if self.prev_regions != regions:
-			view.erase_regions("WordHighlight")
-			if regions:
-				view.add_regions("WordHighlight", regions, Pref.color_scope_name, Pref.draw_outlined)
-			self.prev_regions = regions
+    def highlight_occurences(self, view):
+        regions = []
+        for sel in view.sel():
+            if sel.empty() and Pref.highlight_when_selection_is_empty:
+                string = view.substr(view.word(sel)).strip()
+                if string and all([not c in Pref.word_separators for c in string]):
+                    regions += view.find_all('(?<![\\w])'+re.escape(string)+'\\b')
+            else:
+                word = view.word(sel)
+                if word.end() == sel.end() and word.begin() == sel.begin() :
+                    string = view.substr(word).strip()
+                    if string:
+                        regions += view.find_all('(?<![\\w])'+re.escape(string)+'\\b')
+        if self.prev_regions != regions:
+            view.erase_regions("WordHighlight")
+            if regions:
+                message = str(len(regions)) + ' Occurrences of  "' + string + '"'
+                view.set_status("WordHighlight", message)
+
+                view.add_regions("WordHighlight", regions, Pref.color_scope_name, Pref.draw_outlined)
+            else:
+                view.erase_status("WordHighlight")
+            self.prev_regions = regions
