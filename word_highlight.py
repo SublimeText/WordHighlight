f1c6bf4682df8993db3c5f50c2558fde37199425
Liam Cain <contactliam529@gmail.com>
2011-12-12 19:39:55 -0500

    added mass edit to master; cleared highlight on focus loss

--- word_highlight.py
+++ word_highlight.py
@@ -7,13 +7,13 @@ settings = sublime.load_settings('Word Highlight.sublime-settings')
 
 class Pref:
   def load(self):
-    Pref.color_scope_name                 = settings.get('color_scope_name', "comment")
+    Pref.color_scope_name                   = settings.get('color_scope_name', "comment")
     Pref.selection_delay                    = settings.get('selection_delay', 0.04)
     Pref.draw_outlined                      = bool(settings.get('draw_outlined', True)) * sublime.DRAW_OUTLINED
     Pref.highlight_when_selection_is_empty  = bool(settings.get('highlight_when_selection_is_empty', True))
     Pref.word_separators                    = []
 
-Pref().load();
+Pref().load()
 
 settings.add_on_change('color_scope_name',                  lambda:Pref().load())
 settings.add_on_change('draw_outlined',                     lambda:Pref().load())
@@ -31,11 +31,21 @@ def delayed(seconds):
     return wrapper
   return decorator
 
+class WordHighlightCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    wh = self.view.get_regions("WordHighlight")
+    for w in wh:
+        self.view.sel().add(w)
+
 class WordHighlightListener(sublime_plugin.EventListener):
   prev_regions = []
 
   def on_activate(self, view):
     Pref.word_separators = view.settings().get('word_separators')
+    
+  def on_deactivated(self, view):
+    view.erase_status("WordHighlight")
+    view.erase_regions("WordHighlight")
 
   def on_selection_modified(self, view):
     if not view.settings().get('is_widget'):
@@ -61,9 +71,13 @@ class WordHighlightListener(sublime_plugin.EventListener):
     if self.prev_regions != regions:
       view.erase_regions("WordHighlight")
       if regions:
-        message = str(len(regions)) + ' Occurrences of  "' + string + '"'
-        view.set_status("WordHighlight", message)
         view.add_regions("WordHighlight", regions, Pref.color_scope_name, Pref.draw_outlined)
+        
+        #We read the length of get_regions because duplicate regions are
+        #removed when they're added to the view.
+        occurrences = len(view.get_regions("WordHighlight"))
+        message = str(occurrences) + ' occurrence' + ('s' if occurrences != 1 else '') + ' of "' + string + '"'
+        view.set_status("WordHighlight", message)
       else:
         view.erase_status("WordHighlight")
       self.prev_regions = regions
