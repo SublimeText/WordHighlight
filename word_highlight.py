8f8e67600384958c08fc1068edc72ac4d714c666
evandrocoan <evandrocoan@hotmail.com>
2019-08-09 00:30:47 -0300

    Deprecated the setting highlight_delay, the command
    WordHighlightClick and fixed the word highlighting running on the
    main Sublime Text thread.

--- word_highlight.py
+++ word_highlight.py
@@ -1,11 +1,7 @@
 import re
 import sys
 import time
-
-try:
-    import thread
-except:
-    import _thread as thread
+import threading
 
 import sublime
 import sublime_plugin
@@ -14,6 +10,11 @@ Pref = {}
 settings = {}
 settings_base = {}
 
+g_sleepEvent = threading.Event()
+g_correct_view = None
+g_is_already_running = False
+
+
 def load_settings():
     global settings
     global settings_base
@@ -21,6 +22,7 @@ def load_settings():
 
     if int(sublime.version()) >= 2174:
         settings_base = sublime.load_settings('Preferences.sublime-settings')
+
     else:
         settings_base = sublime.load_settings('Base File.sublime-settings')
 
@@ -32,7 +34,6 @@ def plugin_loaded():
     class Pref:
         def load(self):
             Pref.color_scope_name                                    = settings.get('color_scope_name', "comment")
-            Pref.highlight_delay                                     = settings.get('highlight_delay', 0)
             Pref.case_sensitive                                      = (not bool(settings.get('case_sensitive', True))) * sublime.IGNORECASE
             Pref.draw_outlined                                       = bool(settings.get('draw_outlined', True)) * sublime.DRAW_OUTLINED
             Pref.mark_occurrences_on_gutter                          = bool(settings.get('mark_occurrences_on_gutter', False))
@@ -55,43 +56,83 @@ def plugin_loaded():
 
     settings.add_on_change('HighlightWordsOnSelectionBase', lambda: Pref.load())
     settings_base.add_on_change('HighlightWordsOnSelection', lambda: Pref.load())
-    if Pref.highlight_when_selection_is_empty and not 'running_wh_loop' in globals():
-        global running_wh_loop
-        running_wh_loop = True
-        thread.start_new_thread(wh_loop, ())
+
+    if not g_is_already_running:
+        # unblocks any thread waiting in a g_sleepEvent.wait() call
+        g_sleepEvent.set()
+
+        # Wait last thread Preferences class to be unloaded
+        sublime.set_timeout_async( configure_main_thread, 5000 )
 
 
 def plugin_unloaded():
+    global g_is_already_running
+    g_is_already_running = False
+
+    # unblocks any thread waiting in a g_sleepEvent.wait() call
+    g_sleepEvent.set()
+
     settings_base.clear_on_change('HighlightWordsOnSelection')
     settings_base.clear_on_change('HighlightWordsOnSelectionBase')
 
 
-def wh_loop():
-    while True:
-        sublime.set_timeout(lambda:WordHighlightListener().on_selection_modified(sublime.active_window().active_view() if sublime.active_window() else None), 0)
-        time.sleep(0.3)
+def configure_main_thread():
+    """
+        break/interrupt a time.sleep() in python
+        https://stackoverflow.com/questions/5114292/break-interrupt-a-time-sleep-in-python
+    """
+    global g_is_already_running
+    g_is_already_running = True
 
-# Backwards compatibility with Sublime 2.  sublime.version isn't available at module import time in Sublime 3.
-if sys.version_info[0] == 2:
-    plugin_loaded()
+    # Reset the internal flag to false. Subsequently, threads calling wait() will block until set()
+    # is called to set the internal flag to true again.
+    g_sleepEvent.clear()
+
+    thread = threading.Thread( target=wh_loop )
+    thread.start()
+
+
+def wh_loop():
+    global g_correct_view
+
+    while True:
+        # Stops the thread when the plugin is reloaded or unloaded
+        if not g_is_already_running:
+            break
+
+        if g_correct_view:
+            view = g_correct_view
+
+            g_correct_view = None
+            highlight_occurences( view )
+
+        # Reset the internal flag to false. Subsequently, threads calling wait() will block until set()
+        # is called to set the internal flag to true again.
+        # https://stackoverflow.com/questions/5114292/break-interrupt-a-time-sleep-in-python
+        g_sleepEvent.clear()
+        g_sleepEvent.wait()
 
 
 def escape_regex(str):
     # Sublime text chokes when regexes contain \', \<, \>, or \`.
     # Call re.escape to escape everything, and then unescape these four.
     str = re.escape(str)
+
     for c in "'<>`":
         str = str.replace('\\' + c, c)
     return str
 
 
-class HighlightWordsOnSelectionEnabledCommand(sublime_plugin.ApplicationCommand):
-    def run(self):
+class HighlightWordsOnSelectionEnabledCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
         Pref.enabled = not Pref.enabled
+        view = self.view
+
         if not Pref.enabled:
-            sublime.active_window().active_view().erase_regions("HighlightWordsOnSelection")
+            view.erase_regions("HighlightWordsOnSelection")
+
         else:
-            WordHighlightListener().highlight_occurences(sublime.active_window().active_view())
+            highlight_occurences(view)
 
     def description(self):
         return 'Disable' if Pref.enabled else 'Enable'
@@ -100,6 +141,7 @@ class HighlightWordsOnSelectionEnabledCommand(sublime_plugin.ApplicationCommand)
 class SelectHighlightedWordsCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         wh = self.view.get_regions("HighlightWordsOnSelection")
+
         for w in wh:
             self.view.sel().add(w)
 
@@ -108,10 +150,13 @@ class SelectHighlightedNextWordCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         sel = [s for s in self.view.sel()]
         sel.reverse()
+
         if sel:
             word = sel[0]
             wh = self.view.get_regions("HighlightWordsOnSelection")
+
             for w in wh:
+
                 if w.end() > word.end() and w.end() > Pref.select_next_word_skiped:
                     self.view.sel().add(w)
                     self.view.show(w)
@@ -123,29 +168,25 @@ class SelectHighlightedSkipLastWordCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         sel = [s for s in self.view.sel()]
         sel.reverse()
+
         if sel and len(sel) > 1:
             self.view.sel().subtract(sel[0])
             Pref.select_next_word_skiped = sel[0].end()
-
-
-class WordHighlightClickCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        Pref.select_next_word_skiped = 0
-        view = self.view
-        if Pref.enabled and not view.settings().get('is_widget'):
-            WordHighlightListener().highlight_occurences(view)
 
 
 class WordHighlightListener(sublime_plugin.EventListener):
     def on_activated(self, view):
         Pref.prev_selections = None
         Pref.select_next_word_skiped = 0
+
         if not view.is_loading():
             Pref.word_separators = view.settings().get('word_separators') or settings_base.get('word_separators')
+
             if not Pref.enabled:
                 view.erase_regions("HighlightWordsOnSelection")
 
     def on_selection_modified(self, view):
+        # print('on_selection_modified', view.substr(sublime.Region(0, 10)))
         active_window = sublime.active_window()
         panel_has_focus = not view.file_name()
 
@@ -155,122 +196,148 @@ class WordHighlightListener(sublime_plugin.EventListener):
         # print( "is_widget:", is_widget )
         # print( "panel_has_focus:", panel_has_focus )
         if active_panel and panel_has_focus or is_widget:
+            # print( '1' )
             correct_view = view
 
         else:
+            # print( '2' )
             correct_view = active_window.active_view()
 
-        # print( "correct_view:", correct_view )
         if correct_view and Pref.enabled and not is_widget:
-            now = time.time()
-            if now - Pref.timing > 0.08:
-                Pref.timing = now
-                sublime.set_timeout(lambda:self.highlight_occurences(correct_view), 0)
-            else:
-                Pref.timing = now
+            global g_correct_view
+            g_correct_view = correct_view
 
-    def highlight_occurences(self, view):
-        # print( "view.has_non_empty_selection_region:", view.has_non_empty_selection_region() )
-        if not Pref.highlight_when_selection_is_empty and not view.has_non_empty_selection_region():
-            view.erase_status("HighlightWordsOnSelection")
-            view.erase_regions("HighlightWordsOnSelection")
-            Pref.prev_regions = None
-            Pref.prev_selections = None
-            return
-        # todo: The list cast below can go away when Sublime 3's Selection class implements __str__
-        prev_selections = str(list(view.sel()))
-        # print( "prev_selections:", prev_selections )
-        if Pref.prev_selections == prev_selections:
-            return
-        else:
-            Pref.prev_selections = prev_selections
+            # print( "correct_view:", correct_view )
+            g_sleepEvent.set()
 
-        if view.size() <= Pref.file_size_limit:
-            limited_size = False
-        else:
-            limited_size = True
 
-        # print( 'running'+ str(time.time()) )
+def highlight_occurences(view):
+    # print( "view.has_non_empty_selection_region:", view.has_non_empty_selection_region() )
+    if not Pref.highlight_when_selection_is_empty and not view.has_non_empty_selection_region():
+        view.erase_status("HighlightWordsOnSelection")
+        view.erase_regions("HighlightWordsOnSelection")
+        Pref.prev_regions = None
+        Pref.prev_selections = None
+        return
 
-        regions = []
-        processedWords = []
-        occurrencesMessage = []
-        occurrencesCount = 0
-        for sel in view.sel():
-            if Pref.highlight_when_selection_is_empty and sel.empty():
-                string = view.substr(view.word(sel)).strip()
+    # todo: The list cast below can go away when Sublime 3's Selection class implements __str__
+    prev_selections = str(list(view.sel()))
+
+    # print( "prev_selections:", prev_selections )
+    if Pref.prev_selections == prev_selections:
+        return
+
+    else:
+        Pref.prev_selections = prev_selections
+
+    if view.size() <= Pref.file_size_limit:
+        limited_size = False
+
+    else:
+        limited_size = True
+
+    # print( 'running'+ str(time.time()) )
+    regions = []
+    processedWords = []
+    occurrencesMessage = []
+    occurrencesCount = 0
+
+    for sel in view.sel():
+
+        if Pref.highlight_when_selection_is_empty and sel.empty():
+            string = view.substr(view.word(sel)).strip()
+
+            if string not in processedWords:
+                processedWords.append(string)
+
+                if string and all([not c in Pref.word_separators for c in string]):
+                        regions = find_regions(view, regions, string, limited_size, True)
+
+                if not Pref.highlight_word_under_cursor_when_selection_is_empty:
+
+                    for s in view.sel():
+                        regions = [r for r in regions if not r.contains(s)]
+
+        elif not sel.empty() and Pref.highlight_non_word_characters:
+            string = view.substr(sel)
+
+            if string and string not in processedWords:
+                processedWords.append(string)
+                regions = find_regions(view, regions, string, limited_size, False)
+
+        elif not sel.empty():
+            word = view.word(sel)
+
+            if word.end() == sel.end() and word.begin() == sel.begin():
+                string = view.substr(word).strip()
+
                 if string not in processedWords:
                     processedWords.append(string)
+
                     if string and all([not c in Pref.word_separators for c in string]):
-                            regions = self.find_regions(view, regions, string, limited_size, True)
-                    if not Pref.highlight_word_under_cursor_when_selection_is_empty:
-                        for s in view.sel():
-                            regions = [r for r in regions if not r.contains(s)]
-            elif not sel.empty() and Pref.highlight_non_word_characters:
-                string = view.substr(sel)
-                if string and string not in processedWords:
-                    processedWords.append(string)
-                    regions = self.find_regions(view, regions, string, limited_size, False)
-            elif not sel.empty():
-                word = view.word(sel)
-                if word.end() == sel.end() and word.begin() == sel.begin():
-                    string = view.substr(word).strip()
-                    if string not in processedWords:
-                        processedWords.append(string)
-                        if string and all([not c in Pref.word_separators for c in string]):
-                                regions = self.find_regions(view, regions, string, limited_size, False)
+                            regions = find_regions(view, regions, string, limited_size, False)
 
-            occurrences = len(regions)-occurrencesCount;
-            if occurrences > 0:
-                occurrencesMessage.append('"' + string + '" '+str(occurrences) +' ')
-                occurrencesCount = occurrencesCount + occurrences
-        if Pref.prev_regions != regions:
-            view.erase_regions("HighlightWordsOnSelection")
-            if regions:
-                if Pref.highlight_delay == 0:
-                    view.add_regions("HighlightWordsOnSelection", regions, Pref.color_scope_name, Pref.icon_type_on_gutter if Pref.mark_occurrences_on_gutter else "", sublime.DRAW_NO_FILL if Pref.draw_outlined else 0)
-                else:
-                    sublime.set_timeout(lambda:self.delayed_highlight(view, regions, occurrencesMessage, limited_size), Pref.highlight_delay)
-            else:
-                view.erase_status("HighlightWordsOnSelection")
-            Pref.prev_regions = regions
+        occurrences = len(regions)-occurrencesCount;
 
-    def find_regions(self, view, regions, string, limited_size, is_selection_empty):
-        # to to to too
-        if Pref.highlight_non_word_characters:
-            if Pref.highlight_only_whole_word_when_selection_is_empty and is_selection_empty:
-                search = r'\b'+escape_regex(string)+r'\b'
-            else:
-                search = escape_regex(string)
+        if occurrences > 0:
+            occurrencesMessage.append('"' + string + '" '+str(occurrences) +' ')
+            occurrencesCount = occurrencesCount + occurrences
 
-        else:
-            # It seems as if \b doesn't pay attention to word_separators, but
-            # \w does. Hence we use lookaround assertions instead of \b.
-            if Pref.highlight_only_whole_word_when_selection_is_empty and is_selection_empty:
-                search = r'\b(?<!\w)'+escape_regex(string)+r'(?!\w)\b'
-            else:
-                search = r'(?<!\w)'+escape_regex(string)+r'(?!\w)'
+    if Pref.prev_regions != regions:
+        view.erase_regions("HighlightWordsOnSelection")
 
-        if not limited_size:
-            regions += view.find_all(search, Pref.case_sensitive)
-        else:
-            chars = Pref.when_file_size_limit_search_this_num_of_characters
-            visible_region = view.visible_region()
-            begin = 0 if visible_region.begin() - chars < 0 else visible_region.begin() - chars
-            end = visible_region.end() + chars
-            from_point = begin
-            while True:
-                region = view.find(search, from_point)
-                if region:
-                    regions.append(region)
-                    if region.end() > end:
-                        break
-                    else:
-                        from_point = region.end()
-                else:
-                    break
-        return regions
-
-    def delayed_highlight(self, view, regions, occurrencesMessage, limited_size):
-        if regions == Pref.prev_regions:
+        if regions:
             view.add_regions("HighlightWordsOnSelection", regions, Pref.color_scope_name, Pref.icon_type_on_gutter if Pref.mark_occurrences_on_gutter else "", sublime.DRAW_NO_FILL if Pref.draw_outlined else 0)
+
+        else:
+            view.erase_status("HighlightWordsOnSelection")
+
+        Pref.prev_regions = regions
+
+
+def find_regions(view, regions, string, limited_size, is_selection_empty):
+    # to to to too
+    if Pref.highlight_non_word_characters:
+        if Pref.highlight_only_whole_word_when_selection_is_empty and is_selection_empty:
+            search = r'\b'+escape_regex(string)+r'\b'
+
+        else:
+            search = escape_regex(string)
+
+    else:
+        # It seems as if \b doesn't pay attention to word_separators, but
+        # \w does. Hence we use lookaround assertions instead of \b.
+        if Pref.highlight_only_whole_word_when_selection_is_empty and is_selection_empty:
+            search = r'\b(?<!\w)'+escape_regex(string)+r'(?!\w)\b'
+
+        else:
+            search = r'(?<!\w)'+escape_regex(string)+r'(?!\w)'
+
+    if not limited_size:
+        regions += view.find_all(search, Pref.case_sensitive)
+
+    else:
+        chars = Pref.when_file_size_limit_search_this_num_of_characters
+        visible_region = view.visible_region()
+
+        begin = 0 if visible_region.begin() - chars < 0 else visible_region.begin() - chars
+        end = visible_region.end() + chars
+        from_point = begin
+
+        while True:
+            region = view.find(search, from_point)
+
+            if region:
+                regions.append(region)
+
+                if region.end() > end:
+                    break
+
+                else:
+                    from_point = region.end()
+
+            else:
+                break
+
+    return regions
+
